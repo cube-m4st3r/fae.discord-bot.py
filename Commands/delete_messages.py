65acc48c905e44315c7bb23061d38c2e85b8bb2d
cube-m4st3r <benjaminfinck02@googlemail.com>
2023-05-16 23:06:31 +0200

    added member and number of msg's to response

--- Commands/delete_messages.py
+++ Commands/delete_messages.py
@@ -1,9 +1,14 @@
+import os
+
 import discord
 from discord import app_commands
 from discord.ext import commands
+from dotenv import load_dotenv
 
 import config
 
+
+load_dotenv("settings-docker.env")
 
 class delete_messages(commands.Cog):
 
@@ -14,7 +19,10 @@ class delete_messages(commands.Cog):
     async def deletemessages(self, interaction: discord.Interaction, number: int, member: discord.Member=None):
         if interaction.user.id in config.botConfig["user-access-ids"]:
             delete_counter = 0
-            await interaction.response.send_message(content="Wird gemacht!", delete_after=3, ephemeral=True)
+            if member:
+                await interaction.response.send_message(content=f"{number} message(s) from {member.name} will be deleted!", delete_after=3, ephemeral=True)
+            else:
+                await interaction.response.send_message(content=f"{number} message(s) will be deleted!", delete_after=3, ephemeral=True)
             async for message in interaction.channel.history():
                 if message.author == member or member == None:
                     await message.delete()
@@ -22,7 +30,7 @@ class delete_messages(commands.Cog):
                 if delete_counter == number:
                     break
         else:
-            await interaction.send_message(content="You don't have access to my commands!", ephemeral=True, delete_after=5)
+            await interaction.response.send_message(content="You don't have access to my commands!", ephemeral=True, delete_after=5)
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(delete_messages(bot), guild=discord.Object(id=config.botConfig["hub-server-guild-id"]))
+    await bot.add_cog(delete_messages(bot), guild=discord.Object(id=config.botConfig["hub-server-guild-id"]) or discord.Object(id=os.getenv("GUILD-ID")))
